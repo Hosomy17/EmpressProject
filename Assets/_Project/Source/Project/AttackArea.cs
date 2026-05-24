2ed8a3ae240e54badfb07cbca6ec31c725a5f0b3
eduhlira4 <eduardu.silva@hotmail.com>
2026-05-24 17:22:24 -0400

    Added Scenario Assets and VFXs

--- Assets/_Project/Source/Project/AttackArea.cs
+++ Assets/_Project/Source/Project/AttackArea.cs
@@ -1,18 +1,29 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class AttackArea : MonoBehaviour 
 {
     [SerializeField] private LayerMask m_targetLayer;
-    
+    [SerializeField] private GameObject m_vfxImpactPrefab;
+
     public Action OnHitSuccess;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (((1 << collision.gameObject.layer) & m_targetLayer) != 0)
-        {
-            OnHitSuccess?.Invoke();
-            gameObject.SetActive(false);
-        }
+        if (((1 << collision.gameObject.layer) & m_targetLayer) == 0) return;
+
+        OnHitSuccess?.Invoke();
+        gameObject.SetActive(false);
+
+        var position = collision.ClosestPoint(transform.position);
+        CallVfxImpact(position);
+    }
+
+    private void CallVfxImpact(Vector2 position)
+    {
+        if(m_vfxImpactPrefab == null) return;
+
+        Instantiate(m_vfxImpactPrefab, position, Quaternion.identity);
     }
 }
